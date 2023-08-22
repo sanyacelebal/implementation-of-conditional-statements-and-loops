853491e5a3975b28d8263ee74b52e99c61efb637
om <om@DESKTOP-MGFADB9>
2023-08-22 15:30:22 +0530

    Add project files.

---
+++ Implemenatation on conditional statements and loops/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace Implemenatation_on_conditional_statements_and_loops
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Console.WriteLine("enter 2 no ");
+              int a = int.Parse(Console.ReadLine());
+              int b= int.Parse(Console.ReadLine());
+              if (a > b)
+              {
+                  Console.WriteLine("a is greator");
+              }
+              else if(a < b)
+              {
+                  Console.WriteLine("b is greator");
+              }
+              else
+              {
+                  Console.WriteLine("both are Equals");
+              } 
+            */
+
+
+            /*  int a=3;
+              if(a==3)
+              {
+                  Console.WriteLine("value of  a is 3");
+              }
+              if(a==3)
+              {
+                  Console.WriteLine("value is 3");
+              }
+              else
+              {
+                  Console.WriteLine("value is not 3");
+              }
+            */
+
+
+
+            /*   int a = 5;
+               switch(a)
+               {
+                
+               }
+            
+               Console.WriteLine("hello");  */
+
+            /*   int a = 10;
+               switch (a)
+               {
+                   case 5:
+                   case 10:
+                   case 15:
+                       Console.WriteLine("value is 5");
+                       Console.WriteLine("value is 10");
+                       Console.WriteLine("value is 15");
+                       break;
+                   default:
+                       Console.WriteLine("enter valid choice");
+                       break;
+               }  */
+
+
+
+
+         /*       int i = 0;
+                 for(; ; )
+                 {
+                if (i < 5)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                    break;
+                i++;
+                
+                 }   */
+
+
+
+            /*     while(true)
+                 {
+                     Console.WriteLine("hello");
+                     break;
+                 } */
+
+
+
+         /*     int a = 5;
+               while (a==5)
+               {
+                   Console.WriteLine("hello");
+                   break;
+               }  */
+
+
+
+         /*     int a = 32;
+
+              do
+              {
+                  Console.WriteLine(a);
+                  a++;
+                if (a > 35)
+                    break;
+              } 
+              while (true);   */
+
+
+
+            
+
+        }
+    }
+}
